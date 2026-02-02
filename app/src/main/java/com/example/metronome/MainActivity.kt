6c55f4ba3d9f58f02971d59b5454ba6ffe37a2f3
ryan0 <r.bartelds@student.utwente.nl>
2026-02-02 10:50:01 +0100

    Initial slider

---
+++ app/src/main/java/com/example/metronome/MainActivity.kt
@@ -0,0 +1,115 @@
+package com.example.metronome
+
+import android.R.attr.valueFrom
+import android.os.Bundle
+import android.transition.Slide
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
+import androidx.compose.material3.SliderState
+import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import androidx.compose.ui.unit.dp
+import com.example.metronome.ui.theme.MetronomeTheme
+
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MetronomeTheme {
+                MetronomeApp()
+            }
+        }
+    }
+}
+
+@PreviewScreenSizes
+@Composable
+fun MetronomeApp() {
+    var currentDestination by rememberSaveable { mutableStateOf(AppDestinations.HOME) }
+    var bpmCount by rememberSaveable { mutableFloatStateOf(0f)}
+
+    NavigationSuiteScaffold(
+        navigationSuiteItems = {
+            AppDestinations.entries.forEach {
+                item(
+                    icon = {
+                        Icon(
+                            it.icon,
+                            contentDescription = it.label
+                        )
+                    },
+                    label = { Text(it.label) },
+                    selected = it == currentDestination,
+                    onClick = { currentDestination = it }
+                )
+            }
+        }
+    ) {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Column(modifier = Modifier.padding((innerPadding))) {
+                Column(modifier = Modifier.padding(horizontal = 30.dp)) {
+                    Text(
+                        text = "BPM: " + bpmCount.toInt().toString(),
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
+                    Slider(
+                        value = bpmCount,
+                        steps = 179,
+                        valueRange = 50F..230F,
+                        onValueChange = { bpmCount = it }
+                    )
+                }
+            }
+
+        }
+    }
+}
+
+enum class AppDestinations(
+    val label: String,
+    val icon: ImageVector,
+) {
+    HOME("Home", Icons.Default.Home),
+    FAVORITES("Favorites", Icons.Default.Favorite),
+    PROFILE("Profile", Icons.Default.AccountBox),
+}
+
+@Composable
+fun Greeting(name: String, destination: AppDestinations, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!\n We are on the ${destination.label} page",
+        modifier = modifier
+    )
+}
